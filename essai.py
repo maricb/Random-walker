0846456c496caab0668c45d80fff2fb636dc3dd5
Katell-cmyk <74591414+Katell-cmyk@users.noreply.github.com>
2020-11-19 14:46:00 +0100

    Test absorbing

--- essai.py
+++ essai.py
@@ -8,28 +8,50 @@ from random import randrange
 from matplotlib import pyplot
 
 x=[0]
-N=50
+N = int(input("number of steps : "))
+murdroite = 5
+murgauche = -5
+
+p=(len(x)//2*N)
+
+def absorbing(xn, murdroite, murgauche):
+    xmaxlife=1
+    i=0
+    while xmaxlife != 0 :
+        if xn>murdroite :
+            xn=murdroite
+            xmaxlife==0
+            print("xmaxlife = 0")
+        if xn<murgauche :
+            xn=murgauche
+            xmaxlife == 0
+            print("xmaxlife = 0")
+        return(xmaxlife)
+
 
 for i in range(N):
-    xn = x[-1]+np.random.normal()
-    x.append(xn)
-    if xn <-5 :
-        xn+=1
-    if xn >5:
-        xn-=1
+    xmaxlife=1
+    while xmaxlife !=0 :
+        dx=np.random.normal()
+        xn = x[-1]+ dx
+        print (xn)
+        xn=absorbing(xn, murdroite, murgauche)
+        x.append(xn)
+print (x)
 
 
+#vision horizontale
 plt.subplot(211)
 for i in range(N):
-    plt.plot(x[i],3,'o-b')
+    plt.plot(x[i],3,'ob')
 plt.title("déplacement du bonhomme")
 
+
+#vision en fonction des pas
 plt.subplot(212)
 plt.plot(x,"^y")
 plt.title("distance parcourue a chaque pas")
 plt.xlabel("X")
 plt.ylabel("Y")
-
 plt.show()
-plt.plot(x)
-plt.show()
+#>>>>>>> 52cfbbd8a21549b075332ba65b11eeb6d036c273
--- essai2.py
+++ essai2.py
@@ -17,7 +17,7 @@ x=[0]
 N = int(input("number of steps : "))
 murdroite = 5
 murgauche = -5
-xmaxlife=1
+xmaxlife = 1
 p=(len(x)//2*N)
 #np.random.normal prend une valeur dans une fonction normale centree (je crois hein pas sure du tout les cours de terminale remonte a looooooiiiiin)
 #du coup elle permet de choisir une valeur probable pour la prochaine abscisse du pas
@@ -26,30 +26,48 @@ def reflect(xn,murdroite,murgauche):
     if xn > murdroite :
         xn=murdroite - abs(murdroite - xn)
     if xn < murgauche :
-        xn = murgauche +abs(murgauche - xn)
+        xn = murgauche + abs(murgauche - xn)
     return(xn)
 
+
 def absorbing(xn, murdroite, murgauche):
-    if xn>murdroite :
-        xn=murdroite
-        xmaxlife = 0
-    if xn<murgauche :
-        xn=murgauche
-        xmaxlife = 0
-    return(xn)
+    xmaxlife=1
+    i=0
+    while xmaxlife != 0 :
+        if xn>murdroite :
+            xn=murdroite
+            xmaxlife == 0
+            print("xmaxlife = 0")
+        if xn<murgauche :
+            xn=murgauche
+            xmaxlife == 0
+            print("xmaxlife = 0")
+        return(xmaxlife)
+
+for i in range(N):
+    xmaxlife=1
+    while xmaxlife !=0 :
+        dx=np.random.normal()
+        xn = x[-1]+ dx
+        print (xn)
+        xn=absorbing(xn, murdroite, murgauche)
+        x.append(xn)
+print (x)
 
 
 for i in range(N):
-        dx=np.random.normal()
-        xn = x[-1]+ dx
-        p=np.random.random()
-        if 0.5 < p :
-            xn=reflect(xn, murdroite, murgauche)
-        print (xn)
-        if 0.5 > p :
-            xn = absorbing(xn, murdroite, murgauche)
-        x.append(xn)
+            dx=np.random.normal()
+            xn = x[-1]+ dx
+            p=np.random.random()
+            if 0.5 < p :
+                xn=reflect(xn, murdroite, murgauche)
+            print (xn)
+            if 0.5 > p :
+                xn = absorbing(xn, murdroite, murgauche)
 
+            x.append(xn)
+            print(xmaxlife)
+            print (x)
 
 #vision horizontale
 plt.subplot(211)
