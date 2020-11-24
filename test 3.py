e3acc14793b7b3c9d83145edfd0ce007a782ebc3
Augustoni <60670025+Augustoni@users.noreply.github.com>
2020-11-24 22:24:26 +0100

    moyenne coordonnees temps

--- test 3.py
+++ test 3.py
@@ -14,12 +14,12 @@ seed(1)
 x=[0]
 #nombre de pas fait par la personne alcoolisee
 N = int(input("number of steps : "))
+nbonhomme = int(input("nombre de personne que vous voulez faire marcher : "))
 murdroite = 5
 murgauche = -5
 xmaxlife = 1
-p=(len(x)/2*N)
-ensemblebonhomme = {}
-ensemblebonhommetest =np.zeros([11,N])
+# creation d'une matrice de zeros qui deviendront bientot des heros (oui je sais c'est nul mais je suis fatiguee ok ?)
+stock =np.zeros((nbonhomme,N+1))
 
 #np.random.normal prend une valeur dans une fonction normale centree (je crois hein pas sure du tout les cours de terminale remonte a looooooiiiiin)
 #du coup elle permet de choisir une valeur probable pour la prochaine abscisse du pas
@@ -27,7 +27,7 @@ ensemblebonhommetest =np.zeros([11,N])
 
 
 
-
+# creation de la fonction qui repousse quand on rencontre un mur
 def reflect(xn,murdroite,murgauche):
     if xn > murdroite :
         xn=murdroite - abs(murdroite - xn)
@@ -40,73 +40,75 @@ def absorbing(xn, murdroite, murgauche,xmaxlife):
     if xn>murdroite :
         xn=murdroite
         xmaxlife = 0
-        print("xmaxlife = 0")
+
     if xn<murgauche :
         xn=murgauche
         xmaxlife = 0
-        print("xmaxlife = 0")
+
     return(xn,xmaxlife)
 
-        #for i in range(N):
 
-        #    while xmaxlife !=0 :
-        #        dx=np.random.normal()
-        #        xn = x[-1]+ dx
-        #        print (xn)
-        #        xn=absorbing(xn, murdroite, murgauche)
-        #        x.append(xn)
+def reflectabsord(N,xn,xmaxlife):
+    #on definit p pour qu'il y ait une chance sur deux
+    p=np.random.random()
+    if 0.5 < p :
+        xn=reflect(xn, murdroite, murgauche)
+    if 0.5 > p :
+        xn,xmaxlife = absorbing(xn, murdroite, murgauche,xmaxlife)
+    return(xn,xmaxlife)
 
-plt.subplot(211)
-def reflectabsord(N,x,xmaxlife):
-    for z in range(N):
-        dx=np.random.normal()
-        xn = x[-1]+ dx
-        p=np.random.random()
-        if 0.5 < p :
-            xn=reflect(xn, murdroite, murgauche)
-        if 0.5 > p :
-            xn,xmaxlife = absorbing(xn, murdroite, murgauche,xmaxlife)
-        if xmaxlife!=0:
-            x.append(xn)
 
-for i in range(11) :
+for i in range(nbonhomme) :
+    #il faut remettre a 0 la liste a chaque fois pour que chaque bonhomme commence a 0 pareil pour xmaxlife si le bonhomme est direct mort c'est un peu couillon quand meme
     x = [0]
     xmaxlife=1
+    #p sert a ce qu'il y ait une chance sur 3 pour qu'un bonhomme prenne un chemin ou un autre une fois qu'il choisit une mode il reste dedans
+    p=np.random.choice([1,2,3])
     for z in range(N):
         dx=np.random.normal()
         xn = x[-1]+ dx
-        xn=absorbing(xn, murdroite, murgauche,xmaxlife)
-        if xmaxlife!=0:
-            x.append(xn)
 
-    ensemblebonhomme[i] = x
+        if p==1 :
+            #si on ne fait pas xmaxlife different de 0 alors la personne risque de continuer a bouger alors qu'elle est censee etre absorbee
+            if xmaxlife!=0:
+                xn,xmaxlife=reflectabsord(N,xn,xmaxlife)
+            else:
+                xn,xmaxlife=absorbing(xn, murdroite, murgauche,xmaxlife)
+        if p==2:
+            xn,xmaxlife=absorbing(xn, murdroite, murgauche,xmaxlife)
+        if p==3:
+            xn=reflect(xn,murdroite,murgauche)
+        #ajout a la liste x comprenant les coordonees des pas du bonhomme z
+        x.append(xn)
+    #ajout de la liste des pas finies du bonhomme z dans la matrice
+    stock[i,:] = x
 
-#dictionnaire
-
-sommecoordtot=0
-sommelongliste = 0
-for numerobonhomme in range(11) :
-    x = ensemblebonhomme.get(numerobonhomme)
-    print(x)
-    sommelongliste +=len(x)
-    sommecoordtot+=sum(x)
-    print(sommecoordtot)
-#get = récup valeur associée à la clé 1
+plt.subplot(211)
+for numerobonhomme in range(nbonhomme) :
+    x = stock[numerobonhomme]
     for pas in range(len(x)):
         if numerobonhomme%2==0:
             plt.plot(x[pas],numerobonhomme,'ob')
         else:
             plt.plot(x[pas],numerobonhomme,'or')
         plt.title("déplacement du bonhomme")
-average = sommelongliste/11
-print (average)
 
+
+plt.show
+
+#Des histoires de moyenne de pas
+som=0
+moycoordpas=[]
+plt.subplot(212)
+for pas in range(N):
+    som=0
+    for numerobonhomme in range(nbonhomme) :
+        som+=stock[numerobonhomme][pas]
+    moycoordpas.append(som/(nbonhomme))
+
+plt.plot(moycoordpas,'ob')
 
 #vision en fonction des pas
-plt.subplot(212)
-plt.plot(x,"^y")
-plt.title("distance parcourue a chaque pas")
-plt.xlabel("nombre de pas effectué")
-plt.ylabel("coordonnée du pas")
+
 plt.show()
 #>>>>>>> 52cfbbd8a21549b075332ba65b11eeb6d036c273
